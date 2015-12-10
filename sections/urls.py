10b5ae118a4aff0c0777adb3e543d9b4158d0ae5
EugeneGarbuzov <ridaim@yandex.ru>
2015-12-11 01:14:14 +0300

    Primitive Topics page added.

--- sections/urls.py
+++ sections/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<section_name>[A-Za-z0-9_]+)/$', views.section, name='section')
+    url(r'^(?P<section_name>[A-Za-z0-9_]+)/$', views.section, name='section'),
+    url(r'^(?P<section_name>[A-Za-z0-9_]+)/(?P<topic_name>[A-Za-z0-9_]+)$', views.topic, name='topic')
 ]
--- sections/views.py
+++ sections/views.py
@@ -7,11 +7,22 @@ from django.shortcuts import render
 
 def index(request):
     cursor = connection.cursor()
-    cursor.execute("SELECT Login, Name, sections.Date, Description FROM sections\
-                        JOIN users ON sections.User_ID = users.User_ID")
+    cursor.execute("SELECT Name, Description, sections.Date, Login FROM sections\
+                        JOIN users ON sections.User_ID = users.User_ID;")
     context = {'cursor': cursor}
     return render(request, "index.html", context)
 
 
 def section(request, section_name):
-    return HttpResponse('Not implemented yet.')
+    cursor = connection.cursor()
+    cursor.execute("SELECT topics.Name, topics.Description,  topics.Date, Login, sections.Date FROM topics\
+                        JOIN users ON users.User_ID = topics.User_ID\
+                        JOIN sections ON sections.Section_ID = topics.Section_ID\
+                        WHERE sections.Name = %s;", [section_name])
+    context = {'cursor': cursor}
+    return render(request, "section.html", context)
+
+
+def topic(request, section_name, topic_name):
+    # todo implement
+    return HttpResponse("Not implemented yet.")
