b70465bc2ae4529deb5df6045eb8e16b037ced90
Sector <SteamSector@gmail.com>
2015-12-13 00:50:01 +0300

    kek3

--- app/views.py
+++ app/views.py
@@ -1,8 +1,7 @@
 from django.contrib import auth
 from django.core.urlresolvers import reverse
 from django.db import connection
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from Forum.tools import fetch_to_dict
@@ -12,6 +11,9 @@ from Forum.tools import fetch_to_dict
 
 
 def login(request):
+    if request.user.username:
+        return HttpResponseRedirect(reverse('index'))
+
     if request.method == 'POST':
         login = request.POST['Login']
         password = request.POST['Password']
@@ -37,19 +39,31 @@ def index(request):
 
     if login:
         with connection.cursor() as cursor:
-            cursor.execute('''SELECT Nickname FROM users
-                              WHERE Login = %s;''', login)
-            nickname = cursor.fetchone()[0]
+            cursor.execute('''SELECT u.Nickname, r.Role_Name
+                              FROM users as u, roles as r
+                              WHERE u.Login = %s
+                              AND r.Role_ID = u.Role_ID;''', login)
+            nickname, role = cursor.fetchone()
     else:
         nickname = ''
+        role = 'Regular'
+
+    if role == 'Newbie':
+        role = 'Regular'
 
     with connection.cursor() as cursor:
-        cursor.execute('''SELECT s.Name, s.Description, s.Date, u.Nickname
-                          FROM sections AS s, users AS u
-                          WHERE s.User_ID = u.User_ID;''')
+
+        # todo привести эту дичь к нормальному виду
+
+        cursor.execute('''SELECT s.Name, s.Description, s.Date, u.Nickname,
+                          (SELECT Role_Name FROM roles WHERE roles.Role_ID = s.Role_ID)  as Role_Name
+                          FROM sections AS s, users AS u, roles as r
+                          WHERE s.User_ID = u.User_ID
+                          AND s.Role_ID >= r.Role_ID
+                          AND r.Role_Name = %s;''', role)
         sections = fetch_to_dict(cursor)
 
-    context = {'nickname': nickname,
+    context = {'user': {'nickname': nickname, 'is_admin': role == 'Admin'},
                'sections': sections}
     return render(request, 'index.html', context)
 
@@ -70,5 +84,3 @@ def section(request, section_name):
 def topic(request, section_name, topic_name):
     # todo implement
     return HttpResponse('Not implemented yet.')
-
-
