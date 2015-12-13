43ab3862ef3a2207354f1dd8ad4c99c20e24fbcf
Sector <SteamSector@gmail.com>
2015-12-13 19:11:24 +0300

    добавил удаление

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
+
     url(r'^$', views.index, name='index'),
+    url(r'^remove/(?P<section_name>[A-Za-z0-9_]+)$', views.remove_section, name='remove_section'),
+
     url(r'^(?P<section_name>[A-Za-z0-9_]+)/$', views.section, name='section'),
     url(r'^(?P<section_name>[A-Za-z0-9_]+)/(?P<topic_name>[A-Za-z0-9_]+)$', views.topic, name='topic')
 ]
--- app/views.py
+++ app/views.py
@@ -48,23 +48,39 @@ def index(request):
         nickname = ''
         role = 'Regular'
 
-    role = check_roles(role, 'write')
+    roles = check_roles(role)
 
     with connection.cursor() as cursor:
 
         # todo привести эту дичь к нормальному виду
 
-        cursor.execute('''SELECT s.Name, s.Description, s.Date, u.Nickname,
+        cursor.execute('''SELECT s.Name, s.Description, s.Date,
                           (SELECT Role_Name FROM roles WHERE roles.Role_ID = s.Role_ID)  as Role_Name
-                          FROM sections AS s, users AS u, roles as r
-                          WHERE s.User_ID = u.User_ID
-                          AND s.Role_ID = r.Role_ID
-                          AND r.Role_Name in %s;''', [tuple(role)])
+                          FROM sections AS s, roles as r
+                          WHERE s.Role_ID = r.Role_ID
+                          AND r.Role_Name in %s;''', [tuple(roles)])
         sections = fetch_to_dict(cursor)
 
     context = {'user': {'nickname': nickname, 'is_admin': role == 'Admin'},
                'sections': sections}
     return render(request, 'index.html', context)
+
+
+def remove_section(request, section_name):
+    login = request.user.username
+
+    if login:
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT r.Role_Name
+                              FROM users as u, roles as r
+                              WHERE r.Role_ID = u.Role_ID
+                              AND u.Login = %s;''', login)
+            role, = cursor.fetchone()
+            print(role)
+        if role == 'Admin':
+            with connection.cursor() as cursor:
+                cursor.execute('''DELETE FROM sections WHERE Name = %s''', section_name)
+    return HttpResponseRedirect(reverse('index'))
 
 
 def section(request, section_name):
