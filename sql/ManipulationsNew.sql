a6ee63802482d8e7b1ffbb2bb1ccf5ced05853e8
Sector <SteamSector@gmail.com>
2015-12-15 20:02:55 +0300

    damn

--- sql/ManipulationsNew.sql
+++ sql/ManipulationsNew.sql
@@ -47,31 +47,31 @@ INSERT INTO users
     rank_id,
 
     'user_1',
-    'password',
+    'user',
     'user1@example.com',
 
-    'user_1',
-    'name1 surname1',
+    'user 1',
+    'name surname',
     now(),
-    'status_1',
-    'signature_1'
+    'status',
+    'signature'
 
   FROM roles, ranks
-  WHERE roles.role_name = 'regular' AND ranks.rank_name = 'rank_1'
-  LIMIT 1;
+  WHERE roles.role_name = 'regular' AND ranks.rank_name = 'rank_1';
 
-INSERT INTO users (role_id,
-                   rank_id,
+INSERT INTO users
+(role_id,
+ rank_id,
 
-                   username,
-                   password,
-                   email,
+ username,
+ password,
+ email,
 
-                   nickname,
-                   full_name,
-                   date,
-                   status,
-                   signature)
+ nickname,
+ full_name,
+ date,
+ status,
+ signature)
 
   SELECT
 
@@ -79,18 +79,17 @@ INSERT INTO users (role_id,
     rank_id,
 
     'user_2',
-    'password',
+    'user',
     'user2@example.com',
 
-    'user_2',
-    'name2 surname2',
+    'user 2',
+    'name surname',
     now(),
-    'status_2',
-    'signature_2'
+    'status',
+    'signature'
 
   FROM roles, ranks
-  WHERE role_name = 'newbie' AND rank_name = 'rank_1'
-  LIMIT 1;
+  WHERE role_name = 'newbie' AND rank_name = 'rank_1';
 
 INSERT INTO users
 (role_id,
@@ -124,15 +123,75 @@ INSERT INTO users
   FROM roles, ranks
   WHERE roles.role_name = 'admin' AND ranks.rank_name = 'rank_4';
 
--- update users set password='pas100,./' where login = 'user_1';
--- 
--- update users set status='status_2' where login = 'user_2';
+INSERT INTO users
+(role_id,
+ rank_id,
+
+ username,
+ password,
+ email,
+
+ nickname,
+ full_name,
+ date,
+ status,
+ signature)
+
+  SELECT
+
+    role_id,
+    rank_id,
+
+    'moderator_1',
+    'moderator',
+    'moderator1@example.com',
+
+    'moderator 1',
+    'name surname',
+    now(),
+    'status',
+    'signature'
+
+  FROM roles, ranks
+  WHERE roles.role_name = 'moderator' AND ranks.rank_name = 'rank_2';
+
+INSERT INTO users
+(role_id,
+ rank_id,
+
+ username,
+ password,
+ email,
+
+ nickname,
+ full_name,
+ date,
+ status,
+ signature)
+
+  SELECT
+
+    role_id,
+    rank_id,
+
+    'moderator_2',
+    'moderator',
+    'moderator2@example.com',
+
+    'moderator 2',
+    'name surname',
+    now(),
+    'status',
+    'signature'
+
+  FROM roles, ranks
+  WHERE roles.role_name = 'moderator' AND ranks.rank_name = 'rank_3';
 
 
 INSERT INTO journal (user_id, description, date)
   SELECT
     user_id,
-    'action_1',
+    'action',
     now()
   FROM users
   WHERE username = 'user_1';
@@ -140,80 +199,74 @@ INSERT INTO journal (user_id, description, date)
 INSERT INTO journal (user_id, description, date)
   SELECT
     user_id,
-    'action_2',
+    'action',
     now()
   FROM users
   WHERE username = 'user_2';
 
 
 INSERT INTO sections (role_id, name, date, description) VALUES
-  (3, 'section_1', now(), 'section_1 description'),
-  (4, 'section_2', now(), 'section_2 description');
+  (3, 'section_1', now(), 'description'),
+  (4, 'section_2', now(), 'description');
 
 
 INSERT INTO topics (name, date, description, section_id, user_id)
   SELECT
     'topic_1',
     now(),
-    'description of topic topic_1',
+    'description',
     sections.section_id,
     users.user_id
   FROM sections, users
-  WHERE sections.name = 'section_1' AND users.username = 'user_1'
-  LIMIT 1;
+  WHERE sections.name = 'section_1' AND users.username = 'user_1';
 
 INSERT INTO topics (name, date, description, section_id, user_id)
   SELECT
     'topic_2',
     now(),
-    'description of topic topic_2',
+    'description',
     sections.section_id,
     users.user_id
   FROM sections, users
-  WHERE sections.name = 'section_2' AND users.username = 'user_2'
-  LIMIT 1;
+  WHERE sections.name = 'section_2' AND users.username = 'user_2';
 
 
 INSERT INTO messages (date, text, rating, user_id, topic_id)
   SELECT
     now(),
-    'message_1',
-    1,
+    'message',
+    0,
     users.user_id,
     topics.topic_id
   FROM users, topics
-  WHERE users.username = 'user_1' AND topics.name = 'topic_1'
-  LIMIT 1;
+  WHERE users.username = 'user_1' AND topics.name = 'topic_1';
 
 INSERT INTO messages (date, text, rating, user_id, topic_id)
   SELECT
     now(),
-    'message_1',
-    1,
+    'message',
+    0,
     users.user_id,
     topics.topic_id
   FROM users, topics
-  WHERE users.username = 'user_2' AND topics.name = 'topic_2'
-  LIMIT 1;
+  WHERE users.username = 'user_2' AND topics.name = 'topic_2';
 
 
 INSERT INTO trophies_users (trophy_id, user_id, description)
   SELECT
     trophies.trophy_id,
     users.user_id,
-    'trophy_1 user_1 description.'
+    'description'
   FROM trophies, users
-  WHERE trophies.trophy_name = 'trophy_1' AND users.username = 'user_1'
-  LIMIT 1;
+  WHERE trophies.trophy_name = 'trophy_1' AND users.username = 'user_1';
 
 INSERT INTO trophies_users (trophy_id, user_id, description)
   SELECT
     trophies.trophy_id,
     users.user_id,
-    'trophy_2 user_2 description.'
+    'description'
   FROM trophies, users
-  WHERE trophies.trophy_name = 'trophy_2' AND users.username = 'user_2'
-  LIMIT 1;
+  WHERE trophies.trophy_name = 'trophy_2' AND users.username = 'user_2';
 
 
 INSERT INTO sections_users (section_id, user_id)
@@ -221,16 +274,14 @@ INSERT INTO sections_users (section_id, user_id)
     sections.section_id,
     users.user_id
   FROM sections, users
-  WHERE sections.name = 'section_1' AND users.username = 'user_1'
-  LIMIT 1;
+  WHERE sections.name = 'section_1' AND users.username = 'moderator_1';
 
 INSERT INTO sections_users (section_id, user_id)
   SELECT
     sections.section_id,
     users.user_id
   FROM sections, users
-  WHERE sections.name = 'section_2' AND users.username = 'user_2'
-  LIMIT 1;
+  WHERE sections.name = 'section_2' AND users.username = 'moderator_2';
 
 
 INSERT INTO tags_topics (tag_id, topic_id)
@@ -238,8 +289,7 @@ INSERT INTO tags_topics (tag_id, topic_id)
     tags.tag_id,
     topics.topic_id
   FROM tags, topics
-  WHERE tags.tag_name = 'tag_1' AND topics.name = 'topic_1'
-  LIMIT 1;
+  WHERE tags.tag_name = 'tag_1' AND topics.name = 'topic_1';
 
 
 INSERT INTO tags_topics (tag_id, topic_id)
@@ -247,20 +297,6 @@ INSERT INTO tags_topics (tag_id, topic_id)
     tags.tag_id,
     topics.topic_id
   FROM tags, topics
-  WHERE tags.tag_name = 'tag_2' AND topics.name = 'topic_2'
-  LIMIT 1;
-
-
--- insert into roles_sections(role_id, section_id)
--- 	select roles.role_id, sections.section_id from roles, sections
--- 		where roles.role_name = 'role_1' and sections.name = 'section_1'
---         limit 1;
--- 
--- insert into roles_sections(role_id, section_id)
--- 	select roles.role_id, sections.section_id from roles, sections
--- 		where roles.role_name = 'role_2' and sections.name = 'section_2'
---         limit 1;
-
--- delete from messages where message_id = 1;
+  WHERE tags.tag_name = 'tag_2' AND topics.name = 'topic_2';
 
 COMMIT;
