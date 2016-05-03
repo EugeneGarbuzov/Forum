17f139028c2f527a8fc98745bebebd54c7016af3
Sector <SteamSector@gmail.com>
2016-05-03 12:15:29 +0300

    костыли

--- sql/Functions.sql
+++ sql/Functions.sql
@@ -1,7 +1,11 @@
-CREATE OR REPLACE PROCEDURE log(user_name VARCHAR2, message VARCHAR2)
+CREATE OR REPLACE PROCEDURE log_add(user_name VARCHAR2, message VARCHAR2)
 IS
   BEGIN
-    INSERT INTO journal(user_id, description, entry_date)
-                          SELECT id, message, CURRENT_DATE FROM USERS
-                          WHERE username = user_name;
-  END log;
+    INSERT INTO journal (user_id, description, entry_date)
+      SELECT
+        id,
+        message,
+        CURRENT_DATE
+      FROM USERS
+      WHERE username = user_name;
+  END log_add;
